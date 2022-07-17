8071ba00f6a947b50d390ceacf4ccceb9b873971
zengfung <zengfung@users.noreply.github.com>
2022-07-17 19:59:01 +0000

    Deploying to gh-pages from  @ d451e7d76c13a8ed72eda825ec2b34b3c3b5c62c 🚀

--- src/CompareAlgorithmClassificationRates.jl
+++ src/CompareAlgorithmClassificationRates.jl
@@ -13,7 +13,8 @@ using Wavelets,
       CSV,
       MLJ
 
-include("utils.jl")
+include("mod/utils.jl")
+import .LDBUtils: repeat_experiment, dict2dataframes, aggregate_results
 
 ## ========= Setup =========
 # TODO: Change wavelet filter
@@ -52,13 +53,16 @@ ldbkemd = LocalDiscriminantBasis(wt = wt,
                                  dp = dp,
                                  top_k = k,
                                  n_features = n_features)
-ldbs = Dict("ldbk" => ldbk, "ldbkash" => ldbkash, "ldbkemd" => ldbkemd)
+ldbs = Dict("original" => nothing, "ldbk" => ldbk, "ldbkash" => ldbkash, "ldbkemd" => ldbkemd)
 # TODO: Make hyperparameter changes if necessary
-LDA = @load LDA pkg=MultivariateStats
-CT = @load DecisionTreeClassifier pkg=DecisionTrees
+LDA = @load LDA pkg=MultivariateStats verbosity=0
+CT = @load DecisionTreeClassifier pkg=DecisionTree verbosity=0
 classifiers = Dict("LDA" => LDA(), "CT" => CT())
 # TODO: Make changes to model evaluation measures if necessary
-measures = [MisclassificationRate()]
+measures = [MisclassificationRate(), MulticlassPrecision(), MulticlassTruePositiveRate()]
 
 ## ========== Run experiments ==========
-results = repeat_experiment(ldbs, classifiers, measurs)
+# Set `save_data` to `true` to see the results in csv files.
+results_raw = repeat_experiment(ldbs, classifiers, measures; repeats = 100, save_data=false)
+results_by_measure = Dict("$((string ∘ typeof)(measure))" => dict2dataframes(results_raw, measure; save_data=false) for measure in measures)
+results_aggregate = Dict(key => aggregate_results(value; save_data=false) for (key, value) in results_by_measure)
